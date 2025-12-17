a64da459dbdedae2ec9284c048d80556e91a8e86
Praaathaamesh <prathameshpj222@gmail.com>
2025-12-17 15:39:37 +0530

    Feat: Add first bash script

---
+++ HelloWorld.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Following is the hello world program written in bash script
+# The first line is called "Shabang" which allows executing permisisions to the bash script
+
+echo "Hello, world!"
+
+# make sure to do chmod +x for granting the executable permissions if required
