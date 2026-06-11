e5fbf28164850be1679b950d1811a6ba260c3095
Prathamesh Jadhav <prathameshpj222@gmail.com>
2026-06-11 10:28:57 +0530

    feat: add tuto script storage instruct

---
+++ ScriptStorage.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Learn more about script storage anf linux architecture at: https://www.geeksforgeeks.org/linux-unix/linux-file-hierarchy-structure/
+
+# store the important scripts at /usr/local/bin
+# sudo mv <script.sh> /usr/local/bin/script ; no need to add the extension---linux can handle that
+# sudo chown root:root ; change/grant the ownership to root
+# export PATH=/usr/local/bin:$PATH ; to export the main path at $PATH environment variable
