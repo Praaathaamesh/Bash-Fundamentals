c05d853bb5d2e8c9ce1d1ab20693c7cf39c2d14f
Prathamesh Jadhav <prathameshpj222@gmail.com>
2026-06-11 10:14:16 +0530

    feat/fix: fix/add tuto std input

--- StdIn.sh
+++ StdIn.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# show the directory contents and file numbers when the input is given
-echo "Please suffice the suitable absolute directory path:"
-read -r dirpath;
+read -r name;
 
-fcount=$( (find dirpath | wc -f dirpath) )
-echo "The directory at $dirpath has $fcount files."
+echo "your name is: $name"
