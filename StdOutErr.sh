d64b9aa6f7b8586d74c06e025bd465f570941093
Prathamesh Jadhav <prathameshpj222@gmail.com>
2026-06-12 19:45:21 +0530

    feat/fix: add tuto stdout and stderr, more on ?>

--- StdOutErr.sh
+++ StdOutErr.sh
@@ -5,9 +5,10 @@
     # if -ne 0 : then error executing the cmd
     # bad cmd has $? -ne 0;
 
-# you can redirect these cmd's outputs at specific files using 1 >/>> and 2 >/>>
-    # 1 >/>> <filename.txt> : all the stdouts (good cmd output)
-    # 2 >/>> <filename.txt> : all the stderrs (bad cmd output)
+# you can redirect these cmd's outputs at specific files using 1>/>> and 2>/>>
+    # 1>/>> <filename.txt> : all the stdouts (good cmd output)
+    # 2>/>> <filename.txt> : all the stderrs (bad cmd output)
+    # ?>/>> <filename.txt> : all the stdout and stderr (both cmd output)
 
 # file1 > file2 overwrites the file2 with file1
 # file1 >> file2 appends at the end of file2 withh the content of file1
