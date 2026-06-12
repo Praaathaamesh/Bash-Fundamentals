a456f199e1fc3e81a4d1949aac294dd8a2fd4f2e
Prathamesh Jadhav <prathameshpj222@gmail.com>
2026-06-12 19:13:19 +0530

    feat: add tuto jobschedule cron jobs instructions

--- Jobsheduling.sh
+++ Jobsheduling.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-# using at
+# using "at" package
     # if not installed, install it
     # at <HrHr:MinMin MMDDYY> -f ./<scriptname.sh>
     # atq (cmd) : logs all scheduled jobs
     # atrm <jobnum> (cmd) : removes a specific scheduled job
+
+# using CRON jobs
+    # TIP: always use fully qualified cmds --- cmds with full path (to avoid clash and protect the operations from malicious scripts)
+    # done using "cronfile"; not the original one, but the one stored in "tmp" directory
+    # to edit it go
+    #    cronjob -e -u <specific username> (omit -u for system-wide scheduling)
+    # once in the edit window, find the option structured like this in a line
+    #    MIN    HOUR    DOM     MONTH      DOW      COMMAND
+    #    00-59  00-24   01-31   01-12      01-05    <qualified path to the script>
+    #                                      07 is sunday
