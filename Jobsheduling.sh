f2d387144bf3726b74e12ba0defb9b3c3c56ec8a
Prathamesh Jadhav <prathameshpj222@gmail.com>
2026-06-12 19:05:21 +0530

    feat: add tuto jobschedule

---
+++ Jobsheduling.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# using at
+    # if not installed, install it
+    # at <HrHr:MinMin MMDDYY> -f ./<scriptname.sh>
+    # atq (cmd) : logs all scheduled jobs
+    # atrm <jobnum> (cmd) : removes a specific scheduled job
